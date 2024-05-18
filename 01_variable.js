53acebcbd735a4c1ec67723fe20e64a199310854
samplerpriya <135534888+samplerpriya@users.noreply.github.com>
2024-05-18 21:50:47 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,28 @@
+console.log("Priya Kushwaha")
+
+const accountId = 234254;
+let accountEmail = "priya123@gmail.com"
+var accoundPassword = "12345"
+accountCity = "Jaipur"  // it is possible to use variable before values but it is not a programming practic
+
+// accountId = 23;  it is not possible because caonst is used to store values permanantly, 
+// it will never change after declaration
+
+accountEmail = "aashishkush18@gmmail.com"
+accountPassword = "56780"
+accountCity = "PRAYAGRAJ"
+
+console.log(accountCity)
+console.log(accoundPassword)
+console.log(accountEmail)
+console.log(accountId)
+
+
+/*
+     Prefer not to use var
+     because of issue in block scope and functional scope
+
+*/
+
+
+let accountState ;  // it will give undefined because it is not defined
