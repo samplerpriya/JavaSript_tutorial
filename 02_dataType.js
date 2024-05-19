2774aa22eb3ccb8a357f43c3e79c176079c0c087
samplerpriya <135534888+samplerpriya@users.noreply.github.com>
2024-05-19 08:16:13 +0530

    Add files via upload

---
+++ 02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"  //treat all js code as newer version
+
+// alert("hello")  // we are using vs code not browser
+// semi-colon is not inportant
+
+
+// console(3+4);
+
+
+// DATATYPE IN JAVASCRIPT
+
+/*
+      number -->  2 to power 53
+      bigint -->  
+      string -->  ""
+      boolean --> true/false
+      null --> standalone value
+      undefined --> not defined
+      symbol --> unique
+      object 
+
+*/
+
+// console.log(typeof null) 
+// console.log(typeof undefined)
