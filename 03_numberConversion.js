f6718f8b48cb754943617a597fc1c98ec8f7b6a2
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-05-20 17:18:03 +0530

    Add files via upload

---
+++ 03_numberConversion.js
@@ -0,0 +1,39 @@
+
+// let score = 33;
+// console.log(typeof score);    // gives number
+// console.log(typeof (score));  // gives number
+
+
+// let score = "33";
+// console.log(typeof score);    // gives string
+// console.log(typeof (score));  // gives string
+
+
+// let valueInNumber = Number(score);   // it convert the string to number
+// console.log(typeof (score));
+// console.log(typeof valueInNumber);
+
+
+// let score = "33abc"
+// console.log(typeof score);    // gives string
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber) // It is a special type
+
+// "33" --> 33 ---number 
+// "33asd" --> it will give NaN it types is number
+//  true --> 1;   false --> 0
+
+
+// let isLoggesIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggesIn)
+// console.log(booleanIsLoggedIn)
+// If mention 1 will give true
+// if mention 0 will give false
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
