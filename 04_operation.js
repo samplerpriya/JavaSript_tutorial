a7024bea504f09c5cfea87a7b28aabc222b84a92
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-05-21 21:26:36 +0530

    Add files via upload

---
+++ 04_operation.js
@@ -0,0 +1,64 @@
+//OPERATION
+
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+
+// let str1 ="hello";
+// let str2 = "hitesh";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log("1" + 2 + "2")
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3 );
+
+// console.log(true);
+// console.log(+true)  // it will give 1
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter)
+
+// 
+let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter)
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined > 0 );
+// console.log(undefined == 0 )
+// console.log(undefined < 0 )
+
+// "==="  ---> it will check datatype and values as well
+
+console.log("2" === 2 );
+
+
+
+
+
+// APNA COLLEGE
+
