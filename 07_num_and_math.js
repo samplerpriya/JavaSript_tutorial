61f1d0af6c827d99ab37173f7140af7c20251b56
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-05-26 12:23:31 +0530

    Add files via upload

---
+++ 07_num_and_math.js
@@ -0,0 +1,87 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherName = 23.4755
+// console.log(otherName.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//MIN_VALUE
+//MAX_VALUE
+//MAX_SAFE_INTEGER
+
+//This all i have to study
+
+
+
+
+
+
+
+// SOME METHODS 
+
+
+// 1)  String.prototype.charAt()
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+// let index = 5;
+
+// console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+
+// index = -4;
+
+// console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+
+
+
+// 2) String.prototype.charAt()
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+// const index = 4;
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+
+
+// 3) String.charCodeAt()
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const index = 4;
+console.log(`Character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt( index,)}`,
+);
+
+
+
+
+
+
+// -------------------------MATHS----------------------------
+
+// console.log(Math);  // This is itself a bject
+
+// console.log(Math.abs(-4));  // it convert neg value to positive
+// console.log(Math.round(4.7)); //it round of the number
+// console.log(Math.ceil(4.2)); // it also round of the number but it will choose upper one also
+
+
+// // console.log(Math.floor(4.9));// it alsoround of the number but it choose down one
+
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+// //squareroot
+// //power
+// //
+
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+// const min = 10
+// const max = 20
+
+// Math.random() * (max - min + 1) + min
