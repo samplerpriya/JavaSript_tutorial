b0a6a6ee9791c7247b67bdaa7c4aeae3252df6d6
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-05-30 15:35:39 +0530

    Add files via upload

---
+++ 09_array.js
@@ -0,0 +1,38 @@
+// ARRAY
+
+const myArr = [0,2,3,4,5]
+const myHeors = ["shaktiman" , "naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
+
+
+// Array Methods
+
+// myArr.push(6); 
+// myArr.push(7);// it will add the the number into array
+// myArr.pop();  // it will remove the element from array
+// console.log(myArr);
+
+
+// myArr.unshift(34);   // it will shift the element from 0 index and add 34
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(myArr); 
+// console.log(newArr);
+
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log(("B ", myArr));
+
+
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2);
+
+
+
