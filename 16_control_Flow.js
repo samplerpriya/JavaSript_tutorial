15d13f6587e6c8395bd9e7de1090544241569f16
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-06-07 17:46:03 +0530

    Add files via upload

---
+++ 16_control_Flow.js
@@ -0,0 +1,164 @@
+// //If - else statement
+
+// const isUserLoggedIn = true
+// if(2 == "2"){
+//     console.log("executed");
+
+
+// }
+
+
+// <,>,<=, >=, == , !=, ===
+
+
+// if(2!=3)
+//     {
+//         console.log("Executed");
+//     }
+
+
+
+
+
+
+// const temperature = 41;
+// if(temperature < 50)
+//     {
+//         console.log("less than 50");
+//     }
+//     else
+//     {
+//     console.log("temperature is greater than 50");
+//     }
+//     console.log("Executed");
+
+
+
+
+
+    //  const score = 200
+    //  if(score > 100)
+    //     {
+    //         const power = "fly"
+    //         console.log('user power : ${power}');
+    //     }
+
+    //     console.log('User power : ${power}');
+
+
+
+
+    // const balance = 1000
+    // if(balance > 500) console.log("test"), console.log("test2");
+    //Dont do this type of coding in professional world 
+
+
+
+
+    // const balance = 1000
+    // if(balance >500){
+    //     console.log("less than 500");
+    // }
+    // else if(balance<750){
+    //     console.log("leass than 750")
+    // }
+    // else if(balance<900){
+    //     console.log("less than 750");
+    // }
+    // else{
+    //     console.log("less than 1200");
+    // }
+
+
+
+
+//     const isUserLoggedIn = true
+//     const debitcards = true
+//     const loggedInFromGoogle = false
+//     const loggedInFromEmail = true
+//     if (isUserLoggedIn && debitcards && 2 == 3) {
+//         console.log("Allow to buy course");
+//     }
+//     if(loggedInFromGoogle || loggedInFromEmail)
+//      {
+//           console.log("User  logged in");
+//      }
+
+
+
+
+
+   // switch statement
+//    const month = 3
+
+//    switch (month) {
+//      case 1:
+//           console.log("Jan");
+//           break;
+//      case 2:
+//           console.log("feb");
+//           break;
+//      case 3:
+//           console.log("mar");
+//           break;
+           
+//      default:
+//           break;
+//    }
+
+
+
+
+
+
+   const userEmail = "priyakushw@gmail.com"
+//    if(userEmail ){
+//      console.log("got user email");
+//    }
+//    else {
+//      console.log("dont have user email");
+//    }
+
+
+   //falsy value
+//    false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+   //TRUTHY VALUES
+//    "0",  'false', " ", [], {}, function(){}
+
+
+
+
+// if(userEmail.lenght === 0){
+//      console.log("nArray is empty");
+// }
+
+
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length === 0 )
+//      {
+//           console.log("Object is empty");
+//      }
+
+
+
+
+//      // Nullish coalescing operator (??): null undefined
+//      let val1;
+//      // val1 = 5 ?? 10/
+//      // val2 = null ?? 10
+//      var1 = undefined ?? 15
+// val1 =null ?? 10??20
+//      console.log(val1);
+
+
+
+
+//ternary operator
+
+condition ? true : false
+     
+// ex -
+const iceTeaPrice = 100
+iceTeaPrice<= 80 ? console.log("less than 80 "): console.log("more than 80");
