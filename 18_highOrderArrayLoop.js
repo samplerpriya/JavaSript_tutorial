67dc6e5912e540f3ed67ff8037cd6a60d1701999
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-06-09 07:57:58 +0530

    Add files via upload

---
+++ 18_highOrderArrayLoop.js
@@ -0,0 +1,88 @@
+// for of loop
+// it is a array specific loop
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+// const greetings = "Hello World !"
+// for (const greet of greetings) {
+//     console.log(greet);
+// }
+
+
+
+// Maps
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United State of America")
+// map.set('Fr',"France")
+
+// console.log(map)
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
+// for (const [key, value] of map) {
+    // console.log(key,"-", value);.
+// }
+
+
+
+// objects are not iterable
+// const muObject = {
+//     'game1':'NFS',
+//     'game2':'spiderman'
+// }
+// for (const [key, value] of myObject) {
+//     console.log(ket, "-", value);
+// }
+
+
+
+
+// -------------------------for in ---------------------------
+// const myObject = {
+//     js:'javascript',
+//     cpp:'c++',
+//     rb:"ruby",
+//     swift : "swift by apple"
+// }
+// for (const key in myObject) {
+//     console.log(key);
+//     // console.log(myObject[key]);
+// }
+
+
+
+// const programming = ["js","rb","py","java"]
+// for (const key in programming) {
+//     console.log(key);
+// console.log(programming[key]);
+//     }
+
+
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United State of America")
+// map.set('Fr',"France")
+
+// for (const key in map) {
+//    console.log(key); // it will not run
+// }
+
+
+
+
+// --------------------------------for each----------------------
+
+
+
