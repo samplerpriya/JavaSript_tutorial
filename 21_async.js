6da96a984cde591527aebc41e34f3e02c3dee2c8
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2024-06-12 18:36:25 +0530

    Add files via upload

---
+++ 21_async.js
@@ -0,0 +1,18 @@
+// javascript
+//     Synchrous
+//     Single threaded
+
+
+// Execution Context
+    //   execute one line of code at a time
+    //   console log -- 1
+    //   console.log -- 2
+    // CALL STACK                  // MEMORY HEAP
+
+
+// Blocking code vs non blocking code
+//  Blockig means block the flow of program and read file sync
+
+// Non blocking code means does not block execution and read file async
+
+
