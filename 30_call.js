38d7a2bbfa47603d08155613354dec6f1a8eb6de
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-02-22 08:34:18 +0530

    Add files via upload

---
+++ 30_call.js
@@ -0,0 +1,17 @@
+function SetUsername(username) {
+    // Complex database calls
+
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password) {
+    SetUsername.call(username)
+
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai","chai@fb123","123")
+console.log(chai);
+
