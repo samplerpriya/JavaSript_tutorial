ea105bd79e32488400ab0a1255adcbc889fcbd62
Priya Kushwaha <135534888+samplerpriya@users.noreply.github.com>
2025-03-29 22:47:52 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,9 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+
+// console.log("The final price will be ", pencilPrice+erasorPrice," ruppees");
+
+
+let output = `The total price is: ${pencilPrice+erasorPrice} Rupees`;  //-->template literals
+console.log(output);
+ // template literals - ${}
